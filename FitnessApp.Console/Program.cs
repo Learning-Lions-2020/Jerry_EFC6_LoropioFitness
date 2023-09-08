e527b26d38f379b372c6c8e1057fab6cbd43308c
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2023-09-08 16:12:10 +0300

    find user first name and update

--- FitnessApp.Console/Program.cs
+++ FitnessApp.Console/Program.cs
@@ -8,29 +8,55 @@ using (FitnessAppContext context = new FitnessAppContext())
     context.Database.EnsureCreated();
 }
 
-PrintUser();
+Dialog();
 
-void PrintUser()
+void Dialog()
 {
-    int userIdToFind = 20;
-    User foundUser = FindUserById(userIdToFind);
+    Console.WriteLine("Enter User's first name to find: ");
+    string? firstNameToFind = Console.ReadLine();
+    Console.WriteLine("Enter the new First Name: ");
+    string? newFirstName = Console.ReadLine();
 
-    if (foundUser != null)
-    {
-        Console.WriteLine("Found User!");
-        Console.WriteLine($"User Details: UserId: {foundUser.Id}, First Name: {foundUser.FirstName}, Last Name: {foundUser.LastName}");
-    }
-    else
-    {
-        Console.WriteLine($"User with UserId {userIdToFind} not found.");
-    }
+    FindUserByFirstName(firstNameToFind);
+    UpdateUserFirstName(firstNameToFind, newFirstName);
 }
 
 
-User FindUserById(int userId)
+void FindUserByFirstName(string firstName)
 {
     FitnessAppContext fitnessAppContext = new FitnessAppContext();
 
-    return fitnessAppContext.Users.Find(userId);
+    User user = fitnessAppContext.Users.FirstOrDefault(u => u.FirstName == firstName);
+
+    if (user != null)
+    {
+        Console.WriteLine($"User Details Before Update: FirstName: {user.FirstName}");
+    }
+    else
+    {
+        Console.WriteLine($"User with FirstName '{firstName}' not found.");
+    }
+}
+
+void UpdateUserFirstName(string currentFirstName, string newFirstName)
+{
+    FitnessAppContext fitnessAppContext = new FitnessAppContext();
+
+    User user = fitnessAppContext.Users.FirstOrDefault(u => u.FirstName == currentFirstName);
+
+    if (user != null)
+    {
+        Console.WriteLine($"User Details After Update: FirstName: {user.FirstName}");
+        user.FirstName = newFirstName;
+
+        fitnessAppContext.ChangeTracker.DetectChanges();
+        fitnessAppContext.SaveChanges();
+
+        Console.WriteLine($"User Details After Update: FirstName: {user.FirstName}");
+    }
+    else
+    {
+        Console.WriteLine("User not found for updating FirstName.");
+    }
 }
 
