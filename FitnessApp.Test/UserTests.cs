86b7fe785d29e1587338418c0787871820728b07
Jerry Lokoroi <jerrylokoroi@gmail.com>
2024-03-22 09:43:07 +0300

    Unit Test by verifying that wrong credentials will result in an invalid logon

--- FitnessApp.Test/UserTests.cs
+++ FitnessApp.Test/UserTests.cs
@@ -27,4 +27,25 @@ public class UserTests
         //assert
         Assert.True(logonValid);
     }
+
+    [Fact]
+    public void Test_GetCredentialsAreValid_Returns_False_For_Wrong_Password()
+    {
+        // Arrange
+        var userName = "TestUser";
+        var correctPassword = "TestPassword";
+        var wrongPassword = "WrongPassword";
+        var hash = SecurityProvider.HashPasword(correctPassword, out var salt);
+        var saltForHash = Convert.ToHexString(salt);
+
+        var userRepositoryMock = new Mock<IUserRepository>();
+        userRepositoryMock.Setup(m => m.GetUser(userName)).Returns(new User() { UserName = userName, PasswordHash = hash, PasswordSalt = saltForHash });
+
+        // Act
+        var user = new User(userRepositoryMock.Object);
+        var logonValid = user.GetCredentialsAreValid(userName, wrongPassword);
+
+        // Assert
+        Assert.False(logonValid);
+    }
 }
