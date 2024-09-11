a961e9a0b60ab5ec6c8a732da6ee66629184d32a
Jerry Lokoroi <jerrylokoroi@gmail.com>
2024-09-11 17:41:11 +0300

    created a new folder called IntegrationTests and add three files: SportEventIntegrationTests.cs, TestBase.cs, and UserIntegrationTests.cs.

---
+++ FitnessApp.Test/IntegrationTests/TestBase.cs
@@ -0,0 +1,29 @@
+﻿using FitnessApp.Data.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace FitnessApp.IntegrationTests
+{
+    public  class TestBase 
+    {
+        protected DbContextOptions<FitnessAppContext> Options { get; set; } 
+
+        protected TestBase()
+        {
+            var uiProjectDirectory = Path.Combine(Directory.GetCurrentDirectory(), "..", "..","..", "..", "FitnessApp.API");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(uiProjectDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            Options = new DbContextOptionsBuilder<FitnessAppContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+        }
+    }
+}
+
---
+++ FitnessApp.Test/IntegrationTests/UserIntegrationTests.cs
@@ -0,0 +1,153 @@
+﻿using FitnessApp.Data.DBContext;
+using FitnessApp.Data.Repository;
+using FitnessApp.Domain.CustomTypes;
+using FitnessApp.Domain.Entities.Base;
+using FitnessApp.Domain.Entitities;
+using FitnessApp.IntegrationTests;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace FitnessApp.Test.IntegrationTests
+{
+    public class UserIntegrationTests : TestBase
+    {
+        [Fact]
+        public void GetUser_AddOneUser_ListIsNotEmpty()
+        {
+            using (var _context = new FitnessAppContext(Options))
+            {
+                // Arrange
+                var repository = new UserRepository(_context);
+                var user = new User { UserName = "testUser" };
+
+                // Act
+                repository.AddUser(user);
+                var retrievedUser = repository.GetUser(user.UserName);
+
+                // Assert
+                Assert.NotNull(retrievedUser);
+                Assert.Equal("testUser", retrievedUser.UserName);
+            }
+        }
+
+        [Fact]
+        public void GetUser_AddOneUserWithSportActivity_ActivityListIsNotEmpty()
+        {
+            using (var _context = new FitnessAppContext(Options))
+            {
+                // Arrange
+                var repository = new UserRepository(_context);
+                var user = new User
+                {
+                    UserName = "userWithActivity",
+                    SportActivities = new List<SportActivity>
+                    {
+                        new SportActivity
+                        {
+                            Id = Guid.NewGuid(),
+                            Distance = 10.0,
+                            TimeTaken = TimeSpan.FromHours(1),
+                            ActivityDate = DateTime.Now,
+                            ActivityType = ActivityType.BikeActivity,
+                            Feeling = Feeling.Strong
+                        }
+                    }
+                };
+
+                // Act
+                repository.AddUser(user);
+                var retrievedUser = repository.GetUser(user.UserName);
+
+                // Assert
+                Assert.NotNull(retrievedUser);
+                Assert.NotNull(retrievedUser.SportActivities);
+                Assert.Single(retrievedUser.SportActivities);
+            }
+        }
+
+        [Fact]
+        public void GetUserById_AddOneUserWithSportActivity_ActivityListIsNotEmpty()
+        {
+            using (var _context = new FitnessAppContext(Options))
+            {
+                // Arrange
+                var repository = new UserRepository(_context);
+                var user = new User
+                {
+                    UserName = "userWithActivityById",
+                    SportActivities = new List<SportActivity>
+                    {
+                        new SportActivity
+                        {
+                            Id = Guid.NewGuid(),
+                            Distance = 5.0,
+                            TimeTaken = TimeSpan.FromMinutes(30),
+                            ActivityDate = DateTime.Now,
+                            ActivityType = ActivityType.RunActivity,
+                            Feeling = Feeling.Good
+                        }
+                    }
+                };
+
+                // Act
+                repository.AddUser(user);
+                var retrievedUser = repository.GetUserById(user.Id);
+
+                // Assert
+                Assert.NotNull(retrievedUser);
+                Assert.NotNull(retrievedUser.SportActivities);
+                Assert.Single(retrievedUser.SportActivities);
+            }
+        }
+
+        [Fact]
+        public void GetUserById_AddOneUserWithoutSportActivity_ActivityListEmpty()
+        {
+            using (var _context = new FitnessAppContext(Options))
+            {
+                // Arrange
+                var repository = new UserRepository(_context);
+                var user = new User { UserName = "userWithoutActivity" };
+
+                // Act
+                repository.AddUser(user);
+                var retrievedUser = repository.GetUserById(user.Id);
+
+                // Assert
+                Assert.NotNull(retrievedUser);
+                Assert.NotNull(retrievedUser.SportActivities);
+                Assert.Empty(retrievedUser.SportActivities);
+            }
+        }
+
+        [Fact]
+        public void RemoveAllActivities_RemovesActivities()
+        { 
+            using (var _context = new FitnessAppContext(Options))
+            {
+                // Arrange
+                var repository = new UserRepository(_context);
+                var user = new User
+                {
+                    UserName = "testUser",
+                    SportActivities = new List<SportActivity>
+                    {
+                        new SportActivity { Distance = 5.0, ActivityDate = DateTime.Now }
+                    }
+                };
+                repository.AddUser(user);
+
+                // Act
+                repository.RemoveAllActivities(user.Id);
+                var retrievedUser = repository.GetUserById(user.Id);
+
+                // Assert
+                Assert.NotNull(retrievedUser);
+                Assert.Empty(retrievedUser.SportActivities);
+            }
+        }
+    }
+}
